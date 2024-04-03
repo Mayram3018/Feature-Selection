42f392a5ab1270a79ebad01e43c1fec51009da84
Mayram3018 <nasirianir@gmail.com>
2024-04-03 12:23:40 +0300

    Update FS1.py

--- FS1.py
+++ FS1.py
@@ -1,1 +1,12 @@
-#try to run python file here
+import pandas as pd
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pd.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+
+from sklearn.feature_selection import mutual_info_classif
+importance = mutual_info_classif(X,Y)
+feat_importance = pd.Series(importance, dataframe.columns[0: len(dataframe.columns)-1])
+feat_importance.plot(kind='barh', color='teal')
